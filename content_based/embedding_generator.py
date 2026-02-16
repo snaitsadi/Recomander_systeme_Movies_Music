69a84c75066b83049f0b64109061e7a0a303297d
Samir Nait Sadi <snaitsadi@emi.u-bordeaux.fr>
2026-02-16 14:51:52 +0100

    embedding

---
+++ content_based/embedding_generator.py
@@ -0,0 +1,25 @@
+import pandas as pd
+import numpy as np
+import os
+import pickle
+from sentence_transformers import SentenceTransformer
+from tqdm import tqdm
+
+class SongEmbeddingGenerator:
+    """
+    Generates embeddings for songs based on their metadata using SentenceTransformer.
+    """
+    def __init__(self, model_name="all-MiniLM-L6-v2", data_path="../data/songs_metadata.pkl", output_path="../data/song_embeddings.pkl"):
+        """
+        Initialize the generator.
+        
+        Args:
+            model_name (str): The name of the SentenceTransformer model to use.
+            data_path (str): Path to the pickle file containing song metadata.
+            output_path (str): Path where the generated embeddings will be saved.
+        """
+        self.model_name = model_name
+        self.data_path = data_path
+        self.output_path = output_path
+        self.model = SentenceTransformer(model_name)
+        
