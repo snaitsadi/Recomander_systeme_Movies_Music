058362d045baf61f0a86c9a36fa1db6472eea97e
Samir Nait Sadi <snaitsadi@emi.u-bordeaux.fr>
2026-02-16 14:46:33 +0100

    api.py'

--- collaborative/api.py
+++ collaborative/api.py
@@ -18,3 +18,31 @@ with open(Path(__file__).parent / "../data/songs_metadata.pkl", "rb") as f:
 songs_metadata_indices = set((SONG_MAPPING[song_id] for song_id in songs_metadata["song_id"] if song_id in SONG_MAPPING))
 
 print("[COLLABORATIVE] Dataset ready")
+
+
+from pathlib import Path
+
+SONG_MAPPING_REVERT = {
+    song_index: song_id for song_id, song_index in SONG_MAPPING.items()
+}
+
+model_path = Path(__file__).parent / f"model-{DATASET_SIZE}-{l}"
+q, p, b_song, b_user = load_model(str(model_path))
+
+print("[COLLABORATIVE] Model loaded")
+
+
+def get_recommendations(users_listenings: list[tuple[str, int]]) -> list[str]:
+    print(f"[COLLAB_API] Analyzing {len(users_listenings)} input songs")
+    # User songs as indexes w.r.t. song mapping
+    user_song_indexes = {
+        SONG_MAPPING[song_id]: listening_count
+        for song_id, listening_count in users_listenings
+        if song_id in SONG_MAPPING
+    }
+
+    if not user_song_indexes:
+        print("[COLLAB_API] No valid known songs in input")
+        return []
+    
+    print(f"[COLLAB_API] Found {len(user_song_indexes)} known songs in input")
