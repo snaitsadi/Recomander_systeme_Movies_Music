b9fbcb19576a9c945666314672c2bec6d8136a97
Samir Nait Sadi <snaitsadi@emi.u-bordeaux.fr>
2026-02-16 14:48:59 +0100

    model'

--- collaborative/model.py
+++ collaborative/model.py
@@ -21,3 +21,21 @@ def init(n_songs: int, n_users: int) -> Model:
     b_user = np.random.random_sample(n_users)
 
     return q, p, b_song, b_user
+
+
+
+def save(prefix: str, model: Model):
+    q, p, b_song, b_user = model
+    np.save(prefix + "_q.npy", q)
+    np.save(prefix + "_p.npy", p)
+    np.save(prefix + "_b_song.npy", b_song)
+    np.save(prefix + "_b_user.npy", b_user)
+
+
+def load(prefix: str) -> Model:
+    q = np.load(prefix + "_q.npy")
+    p = np.load(prefix + "_p.npy")
+    b_song = np.load(prefix + "_b_song.npy")
+    b_user = np.load(prefix + "_b_user.npy")
+
+    return (q, p, b_song, b_user)
